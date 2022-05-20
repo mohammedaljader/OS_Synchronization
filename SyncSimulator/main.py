292197d957639dcc53db8b9211714b546fbca4bb
Şindrilaru,Andreea A.D <a.sindrilaru@student.fontys.nl>
2022-05-20 19:52:45 +0200

    updated the directory for week1

--- SyncSimulator/main.py
+++ SyncSimulator/main.py
@@ -4,13 +4,12 @@ import importlib
 import sys
 
 # 'dut_dir': directory where you store your LBoS source files
-dut_dir = ""
-dut_dir = ""
+dut_dir = "./Week1/"
 
 # 'myDut': Device Under Test, a python file with '.py' must exist in 'dut_dir'
 myDut = "Dut_ConditionVariable"
 # myDut = "Dut37_ReusableBarrier_SemOnly"
-myDut = "/Week1/CSynchronization"
+myDut = "CSynchronization"
 # myDut = "Dut44_5_DiningPhilosophers_Tanenbaum"
 # myDut = "Dut44_DiningPhilosophers_CondVar"
 # myDut = "Dut42_ReaderWriter_CondVar"
