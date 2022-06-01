c14b5f774c6fd76b4b9e56e139d19d4e3ce26ae6
Mohammed Aljader <461166@student.fontys.nl>
2022-06-02 00:58:32 +0200

    updated

--- SyncSimulator/Week3/RiverCrossing.py
+++ SyncSimulator/Week3/RiverCrossing.py
@@ -2,56 +2,79 @@ from Environment import *
 
 serfSem = MySemaphore(0, "serfSemaphore")
 hackerSem = MySemaphore(0, "hackerSemaphore")
-totalMultiplex = MySemaphore(5, "totalMultiplex")
-
 barrier = MyBarrier(4, "boatBarrier")
-
+Multiplex = MySemaphore(5, "Multiplex")
 boatMutex = MyMutex("boatMutex")
-initializationMutex = MyMutex("initializationMutex")
-
+SMutex = MyMutex("SMutex")
+HMutex = MyMutex("HMutex")
 serfs = MyInt(0, "SerfCounter")
 hackers = MyInt(0, "HackerCounter")
 passengers = MyInt(0, "PassengerCount")
 
 
-def BoatThread(my_count: MyInt, other_count: MyInt, my_semaphore: MySemaphore, other_semaphore: MySemaphore):
+def SerfsThread():
     while True:
-        totalMultiplex.wait()  # Make sure we have at most 1 boat trips (Simplify problem)
+        Multiplex.wait()
+        SMutex.wait()
 
-        initializationMutex.wait()  # Protect the passenger counts
-        my_count.v += 1
-        if my_count.v == 4:
-            my_semaphore.signal(4)
-            my_count.v = 0
+        serfs.v += 1
+        if serfs.v == 4:
+            serfSem.signal(4)
+            serfs.v = 0
+        elif serfs.v >= 2 and hackers.v >= 2:
+            serfSem.signal(2)
+            hackerSem.signal(2)
+            serfs.v -= 2
+            hackers.v -= 2
 
-        elif my_count.v >= 2 and other_count.v >= 2:
-            my_semaphore.signal(2)
-            other_semaphore.signal(2)
+        SMutex.signal()
+        serfSem.wait()
+        print("Board Serf")
 
-            my_count.v -= 2
-            other_count.v -= 2
-        initializationMutex.signal()
-
-        my_semaphore.wait()  # Let only valid people trough
-
-        boatMutex.wait()  # Protect the boat ride. Nobody can embark when there are people leaving it.
-
-        print("Embark")
+        boatMutex.wait()
         passengers.v += 1
-
         if passengers.v == 4:
-            print("Rowboat")
+            print("rowBoat")
             passengers.v = 0
-
         boatMutex.signal()
 
-        barrier.wait()  # Leave the boat together
-        totalMultiplex.signal()
+        barrier.wait()
+        Multiplex.signal()
+
+
+def HackersThread():
+    while True:
+        Multiplex.wait()
+        HMutex.wait()
+
+        hackers.v += 1
+        if hackers.v == 4:
+            hackerSem.signal(4)
+            hackers.v = 0
+        elif hackers.v >= 2 and serfs.v >= 2:
+            hackerSem.signal(2)
+            serfSem.signal(2)
+            hackers.v -= 2
+            serfs.v -= 2
+
+        HMutex.signal()
+        hackerSem.wait()
+        print("Board Hacker")
+
+        boatMutex.wait()
+        passengers.v += 1
+        if passengers.v == 4:
+            print("rowBoat")
+            passengers.v = 0
+        boatMutex.signal()
+
+        barrier.wait()
+        Multiplex.signal()
 
 
 def setup():
     for i in range(7):
-        subscribe_thread(lambda: BoatThread(serfs, hackers, serfSem, hackerSem))
+        subscribe_thread(SerfsThread)
 
     for i in range(7):
-        subscribe_thread(lambda: BoatThread(hackers, serfs, hackerSem, serfSem))
+        subscribe_thread(HackersThread)
